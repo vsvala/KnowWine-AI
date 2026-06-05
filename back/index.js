eaa405443fcdc8682624a297c38d657b189d96f8
vsvala <virva.svala@gmail.com>
2026-06-05 22:17:06 +0300

    putting path right for index.html

--- back/index.js
+++ back/index.js
@@ -15,7 +15,7 @@ let testdata = [
 
 const path = require('path')
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'dist/dist', 'index.html'))
 })
 
 
