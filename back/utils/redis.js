a830fb876220afffc3bf5ef41acde03dd57c124b
vsvala <virva.svala@gmail.com>
2026-06-24 15:33:51 +0300

    redis for production

--- back/utils/redis.js
+++ back/utils/redis.js
@@ -1,7 +1,13 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: Number(process.env.REDIS_PORT) || 6379,
+      lazyConnect: true,
+    });
+
+redis.on('error', () => {});
+
 module.exports = redis;
